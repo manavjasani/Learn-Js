a7cb2a35dd74a51fdff2e083348cc8736b5df6f4
Manav Jasani <manavjasani12@gmail.com>
2021-07-02 17:59:29 +0530

    Solve javascript array-problems

--- js_methods-master/array-methods/1-array.js
+++ js_methods-master/array-methods/1-array.js
@@ -3,48 +3,119 @@ var numbers = [1, 12, 4, 18, 9, 7, 11, 3, 101, 5, 6];
 var strings = ["this", "is", "a", "collection", "of", "words"];
 
 // Use the above two arrays and practice array methods
+  numbers.push(5);
+  numbers.pop();
+  strings.unshift("javascript");
+  strings.shift();
 
-// Find largest number in numbers
+// Find largest number in numbers 
+  let largeNo = 0;
+  for(let i = 0; i < numbers.length; i++) {
+    numbers[i] > largeNo ? largeNo = numbers[i] : largeNo;
+  }
 
 // Find longest string in strings
+  let strlength = 0;
+  let longest;
+  for(let i = 0; i < strings.length; i++) {
+    if(strings[i].length > strlength) {
+      longest = strings[i];
+      strlength = strings[i].length;
+    } 
+  }
 
 // Find all the even numbers
+const evenNo = numbers.filter((num) => { 
+  if(num % 2 == 0) return num;
+});
 
 // Find all the odd numbers
+const oddNo = numbers.filter(num => {
+  if(num % 2 == 1) return num;
+});
 
 // Find all the words that contain 'is' use string method 'includes'
+  let arr = [];
+  for(let i = 0; i < strings.length; i++) {
+    if(strings[i].includes('is')) {
+      arr.push(strings[i]);
+    }
+  }
 
 // Find all the words that contain 'is' use string method 'indexOf'
+  let word = strings.map(val => {
+      if(val.indexOf('is')) {
+        return val;
+      }
+    });
+    console.log(word);
+
 
 // Check if all the numbers in numbers array are divisible by three use array method (every)
-
+  numbers.every(num => num % 3 === 0);
+  
 //  Sort Array from smallest to largest
+  numbers.sort((a, b) => a - b);
 
 // Remove the last word in strings
+  strings.pop();
 
 // Add a new word in the array
+  strings.push('words');
 
 // Remove the first word in the array
+  strings.shift();
 
 // Place a new word at the start of the array use (upshift)
+  strings.unshift('this');
 
 // Make a subset of numbers array [18,9,7,11]
+  numbers.slice(3, 7);
 
 // Make a subset of strings array ['a','collection']
+  strings.slice(2, 4);
 
 // Replace 12 & 18 with 1221 and 1881
+  numbers.splice(8, 1, 1221);
+  numbers.splice(9, 1, 1881);
 
 // Replace words with string in strings array
+  strings.splice(5, 1, 'string');
 
 // Customers Array
-var customers = [
-  { firstname: "Joe", lastname: "Blogs" },
-  { firstname: "John", lastname: "Smith" },
-  { firstname: "Dave", lastname: "Jones" },
-  { firstname: "Jack", lastname: "White" }
-];
+  var customers = [
+    { firstname: "Joe", lastname: "Blogs" },
+    { firstname: "John", lastname: "Smith" },
+    { firstname: "Dave", lastname: "Jones" },
+    { firstname: "Jack", lastname: "White" }
+  ];
 // Find all customers whose firstname starts with 'J'
+  customers.filter(val => {
+    const arr = val.firstname;
+    if(arr[0] !== 'J') return val = [];
+  })
 
 // Create new array with firstname and lastname
+  let fName = [];
+  let lName = [];
+  customers.map(val => {
+    fName.push(val.firstname);
+  });
+  customers.map(val => {
+    lName.push(val.lastname);
+  });
 
 // Sort the array created above alphabetically
+  
+  customers.sort(function(a, b) {
+    var nameA = a.firstname.toUpperCase(); 
+    var nameB = b.lastname.toUpperCase(); 
+    if (nameA < nameB) {
+      return -1;
+    }
+    if (nameA > nameB) {
+      return 1;
+    }
+  
+    return 0;
+  });
--- js_methods-master/array-methods/2-array.js
+++ js_methods-master/array-methods/2-array.js
@@ -9,7 +9,17 @@ var words = [
 ];
 //Write a function findLongestWord that takes an array of words and returns the longest one.
 //If there are 2 with the same length, it should return the first occurrence.
-
+let longWord;
+let long = 0;  
+const longest = function(arr) {
+    for(let i = 0; i < arr.length; i++) {
+      if(arr[i].length > long) {
+        long = arr[i].length;
+        longWord = arr[i];
+      }
+    }
+    return longWord;
+  } 
 
 
 
@@ -17,13 +27,24 @@ var numbers1 = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 // Create a sumArray function that takes an array of numbers1 as a parameter, and calculate the sum of all its numbers
 // Use reduce method of array
 // Use the above sum and calculate the average.
-
+const sumArray = function(arr) {
+  const red = arr.reduce((acc, move) => {
+    return acc += move;
+  }, arr[0]);
+  return red;
+}
+const avg = sumArray(numbers1) / numbers1.length;
 
 
 
 var numbers2 = [2, 6, 9, 10, 7, 4, 1, 9];
 //Write a function averageNumbers that receives an array of numbers2 and calculate the average of the numbers
-
+const averageNumbers = function(arr) {
+  const avg = arr.reduce((acc, move) => {
+      return acc += move;
+  }, arr[0]);
+    return avg / arr.length;;
+}
 
 
 
@@ -41,5 +62,10 @@ var words2 = [
 ];
 //Write a function averageWordLength that receives an array of words2 and calculate the average length of the words.
 
-
-
+const averageWordLength = function(arr) {
+let length = 0;
+for(let i = 0; i < words2.length; i++) {
+      length += words2[i].length;
+    }
+     return length / words2.length;
+}
